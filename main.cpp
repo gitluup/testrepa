5fb52f4ec9be3370b99fd90b01eac7356b076d9a
gitluup <artem.mikhalev@phystech.edu>
2017-03-03 16:52:46 +0300

    unwanted commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-// comment
 
 int main()
 {
-	std::cout << "Hello from grozzmaster!" << std::endl;
-	int a = 0;
-	std::cout << "Hello world!" << std::endl;
-	int b = 0;
-	++b;
-
-	std::cout << "Hello from gitluup!" << std::endl;
-	return 1;
+	
+	return 0;
 }
