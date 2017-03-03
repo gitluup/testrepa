63d705a183198c0b8b36a4a11d1a018a765fc791
grozzmaster <grozzmaster@users.noreply.github.com>
2017-03-03 13:32:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-// comment
 
 int main()
 {
-	std::cout << "Hello from grozzmaster!" << std::endl;
-	int a = 0;
-	std::cout << "Hello world!" << std::endl;
-	int b = 0;
-	++b;
-
-	a = 10;
-	std::cout << "Hello from gitluup!" << std::endl;
-	a += 3;
-	return 1;
+	return 2;
 }
